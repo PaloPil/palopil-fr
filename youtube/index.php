3e81b8d9db94b662cfa1c34152c7866476c9c668
PaloPil <paul.rabste@gmail.com>
2024-04-01 00:35:25 +0200

    Add GitHub link to contact information

---
+++ youtube/index.php
@@ -0,0 +1,22 @@
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <title>Redirect</title> 
+        
+        <meta property="og:type" content="video.other">
+
+        <meta name="twitter:player" content="https://www.youtube.com/embed/dQw4w9WgXcQ"> 
+        <meta name="og:video:width" content="1920"> 
+        <meta name="og:video:height" content="1080"> 
+        <meta http-equiv="refresh" content="0; url=https://www.youtube.com/watch?v=dQw4w9WgXcQ">
+        <script type="text/javascript">
+            window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+        </script>
+
+        <meta name="twitter:image" content="https://i.imgur.com/<?php echo $_GET['link']; ?>.png">
+    </head> 
+    <body> 
+        <p>Redirecting to <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">https://www.youtube.com/watch?v=dQw4w9WgXcQ</a></p>
+    </body>
+</html>
+
